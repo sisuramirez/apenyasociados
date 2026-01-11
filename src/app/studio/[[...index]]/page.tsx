36d795cc1f5f59347853b473305709aa5f80eeab
Saulo Ramirez <sj.ramirezsuruy@gmail.com>
2026-01-10 23:11:29 -0600

    Version final para hacer deploy en vercel

--- src/app/studio/[[...index]]/page.tsx
+++ src/app/studio/[[...index]]/page.tsx
@@ -6,3 +6,6 @@ import config from '../../../../sanity.config'
 export default function StudioPage() {
   return <NextStudio config={config} />
 }
+
+// Prevent static generation for this route
+export const dynamic = 'force-dynamic'
